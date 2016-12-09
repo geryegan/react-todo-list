ea41370e39c2867a8d5e9d759cfaca49b61f6664
Gery Egan <gery.egan@gmail.com>
2016-12-08 17:31:06 -0800

    webpack hot reload implemented

--- components/App.js
+++ components/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
 class App extends React.Component {
+	clickHandle(){
+		console.log('lolol')
+	}
 	render() {
-		return(<div> This is definitely a React App now! </div>)
+		return(
+			<div>
+			 Hello from React!
+			 	<div>
+			 		<button onClick = {this.clickHandle.bind(this)}>Woo!</button>
+			 	</div>
+			 </div>
+			)
 	}
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
+var webpack = require('webpack');
+
 module.exports = {
-	entry: ['./client/client.js'],
+	devtool: 'inline-source-map',
+	entry: ['webpack-hot-middleware/client',
+	'./client/client.js'],
 	output: {
-		path: './dist',
+		path: require('path').resolve('./dist'),
 		filename: 'bundle.js',
 		publicPath: '/'
 	},
+	plugins: [
+		new webpack.optimize.OccurenceOrderPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoErrorsPlugin()
+	],
 	module: {
 		loaders: [
 			{
@@ -12,7 +21,7 @@ module.exports = {
 				loader: 'babel-loader',
 				exclude: /node_modules/,
 				query: {
-					presets: ['react', 'es2015']
+					presets: ['react', 'es2015','react-hmre']
 				}
 			}
 		]
